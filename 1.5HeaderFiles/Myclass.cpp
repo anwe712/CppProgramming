25b1ca61402ee348dcde06d46574635e7b54e1eb
anwe712 <ghoshanwesha272@gmail.com>
2025-04-12 12:59:30 +0530

    changes

--- 1.5HeaderFiles/Myclass.cpp
+++ 1.5HeaderFiles/Myclass.cpp
@@ -1,16 +1,43 @@
-#include "Myclass.h" //user defined header file
+#include "Myclass.h" // user-defined header file
 #include <iostream>
-using namespace std ;
+#include <cmath> // for pow function
+using namespace std;
 
-void Myclass::printMessage()
+void checkArmstrong(int num)
 {
-    cout << "Hello from MyClass!" << endl;
-}
+    int originalNum = num, sum = 0, digits = 0;
 
+    // Calculate the number of digits
+    while (originalNum != 0) {
+        digits++;
+        originalNum /= 10;
+    }
+
+    originalNum = num;
+
+    // Calculate the sum of the digits raised to the power of 'digits'
+    while (originalNum != 0) {
+        int digit = originalNum % 10;
+        sum += pow(digit, digits);
+        originalNum /= 10;
+    }
+
+    // Check if the number is an Armstrong number
+    if (sum == num) {
+        cout << num << " is an Armstrong number." << endl;
+    } else {
+        cout << num << " is not an Armstrong number." << endl;
+    }
+}
 
 int main() {
     Myclass obj;
-    obj.printMessage();
+    int num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    obj.checkArmstrong(num);
+
     return 0;
 }
-
